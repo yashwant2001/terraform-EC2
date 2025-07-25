4559ff340a666b26978d6c3c26be69b36df1bbf3
Yashwant Patil <97736032+yashwant2001@users.noreply.github.com>
2025-07-25 12:40:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,19 +1,1 @@
-terraform {
-  required_providers {
-    aws = {
-      source = "hashicorp/aws"
-      version = "6.5.0"
-    }
-  }
-}
 
-
-
-resource "aws_instance" "web" {
-  ami           = "ami-08a6efd148b1f7504"
-  instance_type = "t2.micro"
-  key_name      = "my-key"
-  tags = {
-    Name = "web"
-  }
-}
