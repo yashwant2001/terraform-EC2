4a8c56669a673c6daca3108c20f89e864502d773
yashwant2001 <yashpp90@gmail.com>
2025-07-25 12:44:49 +0530

    new changes

--- main.tf
+++ main.tf
@@ -8,7 +8,6 @@ terraform {
 }
 
 
-
 resource "aws_instance" "web" {
   ami           = "ami-08a6efd148b1f7504"
   instance_type = "t2.micro"
--- output.tf
+++ output.tf
@@ -1,3 +1,4 @@
+
 output "instance_ip" {
   value = aws_instance.web.public_ip
 }
