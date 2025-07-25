c48968b15babffb66e94e7e47c933777638dd444
yashwant2001 <yashpp90@gmail.com>
2025-07-25 12:54:13 +0530

    new

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.5.0"
+    }
+  }
+}
 
 resource "aws_instance" "web" {
   ami           = "ami-08a6efd148b1f7504"
